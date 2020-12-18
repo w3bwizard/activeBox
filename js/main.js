8e9b26bed3850b3295830ce4bdb8caf34f3ab9c9
w3bwizard <w3bwiz4rd@gmail.com>
2020-12-19 01:14:58 +0200

    Оживил слайдер

--- js/main.js
+++ js/main.js
@@ -13,12 +13,13 @@ $(function () {
     $(window).on("scroll resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
-
+        console.log(introH);
+        console.log(scrollPos);
         checkScroll(scrollPos, introH);
     });
 
     function checkScroll(scrollPos, introH) {
-        if (scrollPos > introH) {
+        if (scrollPos >= introH - 62) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
@@ -34,15 +35,38 @@ $(function () {
 
         nav.removeClass("show");
 
+
         $("html, body").animate({
             scrollTop: elementOffset - 60
         }, 700);
     });
 
-    /* Показать/скрыть верхнее меню */
+    /* Бургер: показать/скрыть верхнее меню */
     navToggle.on("click", function (event) {
         event.preventDefault();
 
         nav.toggleClass("show");
+    });
+
+    /* Клик в любое место: скрыть верхнее меню */
+    $(document).mouseup(function (e) { // событие клика по веб-документу
+        var menu = $("#header__menu"); // тут указываем ID элемента
+        var burger = $("#navToggle");
+        if (!menu.is(e.target), !burger.is(e.target) // если клик был не по нашему блоку
+            && menu.has(e.target).length === 0) { // и не по его дочерним элементам
+            nav.removeClass("show");// скрываем его
+        }
+    });
+
+    /* Слайдер: https://kenwheeler.github.io/slick/ */
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        arrows: false,
+        dots: true
     });
 });
